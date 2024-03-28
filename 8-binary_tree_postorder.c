476d8b94ec26fd32ee667e451a6bb20b72b24b9d
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-03-29 00:48:38 +0300

    I am not one for relationships, it's over rated, but we can still be lovers.

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - this function goes through a binary tree using
+ * post-order traversal.
+ *
+ * @tree: this is a pointer to the root node of the tree to traverse.
+ * @func: this is a pointer to a functino to call for each node. The value in
+ * the node must be passed as a parameter to this function.
+ *
+ * Description: refer to the first commented out line
+ * (+)
+ * if tree or func is NULL, do nothing
+ *
+ * Return: nada
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
